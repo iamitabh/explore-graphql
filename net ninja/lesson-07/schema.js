f061ebb107f364a3dd5b58ea8c50fc2ac9f8e79c
iamitabh <amitabh.mishra.2002@gmail.com>
2024-06-21 10:25:48 +0530

    add lesson 07 of net ninja

--- net ninja/lesson-07/schema.js
+++ net ninja/lesson-07/schema.js
@@ -3,11 +3,14 @@ export const typeDefs = `#graphql
         id: ID!
         title: String!
         platform: [String!]!
+        reviews: [Review!]
     }
     type Review {
         id: ID!
         rating: Int!
         content: String!
+        game: Game!
+        author: Author!
     }
     type Author {
         id: ID!
